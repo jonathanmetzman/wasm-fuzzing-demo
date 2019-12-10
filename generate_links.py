63bd0732215a5c0f38ca47adb9999cd0d19553fb
Jonathan Metzman <metzman@chromium.org>
2019-12-10 11:29:05 -0800

    add generate_links.py

---
+++ generate_links.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+def find_html(directory):
+    for filename in os.listdir(directory):
+        path = os.path.join(directory, filename)
+        if path.endswith('.html'):
+            yield path
+
+
+def get_links(paths):
+    links = []
+    for path in paths:
+        name, _ = os.path.splitext(path)
+        links.append(f'<a href="/{path}">{name}</a>')
+    return links
+
+def main():
+    directory = sys.argv[1]
+    paths = find_html(directory)
+    print('\n'.join(get_links(paths)))
+
+
+if __name__ == '__main__':
+    main()
